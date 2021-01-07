171b2982b7cfbf689601e80ac99dcad531cdf906
Rene Wang <rene.wang1@hotmail.com>
2021-01-07 03:18:10 +0000

    parameterized queries in students and teachers

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-const input = process.argv.slice(2);
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 INNER JOIN assistance_requests ON teacher_id = teachers.id
 INNER JOIN students ON student_id = students.id
 INNER JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${input[0]}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`;
+
+const cohortName = process.argv[2];
+const values = [`${cohortName}`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`)
